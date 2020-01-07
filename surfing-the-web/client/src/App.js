df968a17f3817093e5a7a05e98b7ae4df545a808
Xavier Davis <xdavis1@icloud.com>
2020-01-07 11:14:19 -0800

    I am creating our MVC folder structure

---
+++ surfing-the-web/client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// We dont have to use app.css just let me know if you need me to delete that file.
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ surfing-the-web/client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Just like the the app.js if you dont need the style sheet for index I can delete the file
+
+// import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
